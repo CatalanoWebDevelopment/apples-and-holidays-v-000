04878a39827c8f00e484fc344177d594fbd2278a
Parker Catalano <catalano.work@gmail.com>
2017-10-30 17:58:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  new_array = []
   holiday_hash.each do |season, holiday|
     holiday.each do |event, supplies|
       if supplies.include?("BBQ")
